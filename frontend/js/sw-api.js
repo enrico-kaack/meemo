4d916082d4b6fd5a5ed15bffc14fb28b2d8152f1
Enrico <blackfire185@gmail.com>
2017-06-19 22:32:03 +0200

    Implemented handling for non filtered get requests

--- frontend/js/sw-api.js
+++ frontend/js/sw-api.js
@@ -11,7 +11,8 @@ self.addEventListener('fetch', function(event) {
             fetch(event.request)
                 .then(function (response) {
                     response.json().then(function (json) {
-                        cacheFirstData(json.token);
+                        storeValue('token', json.token);
+                        fetchNewItems(json.token);
                     });
                     return;
                 })
@@ -25,25 +26,167 @@ self.addEventListener('fetch', function(event) {
 
 });
 
-function cacheFirstData(token){
-    fetch('/api/things?token=' + token + '&skip=0&limit=100', {mode: 'cors'})
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(json) {
-            console.log(json);
-            cacheThings(json);
-        })
-        .catch(function(error) {
-            console.log('Request failed', error);
+
+self.addEventListener('fetch', function(event) {
+    if (event.request.url.match(/\api\/things*/g) && event.request.method === 'GET'){
+        var params = decodeUrlForParameter(event.request.url);
+        var skip = isNaN(parseInt(params.skip)) ? 0 : parseInt(params.skip);
+        var limit = isNaN(parseInt(params.limit)) ? 10 : parseInt(params.limit);
+        var filter = params.filter;
+        if (filter === null || filter === ''){
+            event.respondWith(
+                getAll(skip, limit)
+
+
+            );
+        }else{
+            //search(filter, skip, limit);
+        }
+
+
+
+    }
+
+});
+
+function decodeUrlForParameter(url){
+    var vars = {};
+    var parts = url.replace(/[?&]+([^=&]+)=([^&]*)/gi,
+        function(m,key,value) {
+            vars[key] = value;
         });
+    console.log('params', vars);
+    return vars;
+}
+
+function search(filterString, skip, limit) {
+    //importScripts('../3rdparty/js/pouchdb.quick-search.js');
+
+    //PouchDB.plugin(quick-search);
+    var db = new PouchDB('things');
+    db.search({
+        query: filterString,
+        fields: ['richContent'],
+        include_docs: true
+    }).then(function (result) {
+        console.log(result)
+    }).catch(function (err) {
+        console.error(err)
+    })
 }
 
 
-function cacheThings(data) {
+function getAll(skip, limit) {
     var db = new PouchDB('things');
-    db.bulkDocs(data.things);
 
+    return promise = new Promise(function(resolve, reject) {
+        db.allDocs({
+            include_docs: true,
+            skip: skip,
+            limit: limit
+        }).then(function (result) {
+            var thingList = [];
+            console.log(result)
+            result.rows.forEach(function (thing, item, array) {
+                thingList.push(thing.doc);
+            });
+
+            resolve(new Response(JSON.stringify({things: thingList}), {
+                headers: {
+                'Content-Type':'application/json'
+            }
+            }));
+        }).catch(function (err) {
+            reject(Error(err));
+        });
+    });
+
+
+}
+
+
+function fetchNewItems(token){
+    getStoredValue('lastSync', function (lastSync) {
+        getStoredValue('token', function (token) {
+            fetch('/api/new?token=' + token + '&lastSync=' + lastSync, {mode: 'cors'})
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(json) {
+                    updateLocalDatabse(json.things);
+                })
+                .catch(function(error) {
+                    console.log('Request failed', error);
+                });
+        });
+    });
+
+}
+
+function updateLocalDatabse(data) {
+    var db = new PouchDB('things');
+    storeValue('lastSync', new Date().getTime());
+
+    data.forEach(function (thing, index, array) {
+        updateSingleThing(db, thing);
+    });
+
+
+}
+
+function updateSingleThing(db, thing) {
+console.log(thing)
+    db.get(thing._id).then(function(doc) {
+        thing._rev = doc._rev;
+        return db.put(thing);
+    }).then(function(response) {
+        console.log("Success updating thing", response);
+    }).catch(function (err) {
+        if (err.status === 404){
+            db.put(thing).then(function (response) {
+                console.log('Success inserting thing', response)
+            }).catch(function (err) {
+                console.log('Error inserting thing', err);
+            });
+        }
+    });
+}
+
+
+//store the token in the indexed db, cant use local storage in service worker
+function storeValue(key, value){
+    var db = new PouchDB('sw-settings');
+    db.get(key).then(function(doc) {
+        return db.put({
+            _id: key,
+            _rev: doc._rev,
+            value: value
+        });
+    }).then(function(response) {
+        console.log("Success updating ", key, response);
+    }).catch(function (err) {
+        if (err.status === 404){
+            db.put({
+                _id: key,
+                value: value
+            }).then(function (response) {
+                console.log('Success inserting',key , response)
+            }).catch(function (err) {
+                console.error('Error inserting ', key, err);
+            });
+        }
+    });
+}
+
+function getStoredValue(key, callback){
+    var db = new PouchDB('sw-settings');
+    db.get(key).then(function(response) {
+        console.log("Success loading saved ", key, response);
+        callback(response.value);
+    }).catch(function (err) {
+        console.error('Error loading saved', key, err);
+        callback(null);
+    });
 }
 
 
